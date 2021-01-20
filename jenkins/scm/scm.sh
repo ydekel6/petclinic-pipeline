e237791ad43c5b05d1b5c1889a0f8deb38557f69
Yuval Dekel <ydekel6@gmail.com>
2021-01-20 13:58:40 +0000

    added the scm script

--- jenkins/scm/scm.sh
+++ jenkins/scm/scm.sh
@@ -4,6 +4,10 @@
 
 git clone https://github.com/spring-projects/spring-petclinic.git
 
+echo "***************************"
+echo "** Building jar ***********"
+echo "***************************"
+
 # WORKSPACE=/home/jenkins/jenkins-data/
 
 # docker run --rm  -v  $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
